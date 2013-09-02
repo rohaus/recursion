9f0556eec3f4dc2e1d7c7f7a15ee0d44c2223263
Ryan Roxas <ryanaroxas@gmail.com>
2013-09-01 23:07:52 -0700

    Added 'stringifyJSON' function. Passed all tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,41 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  var result;
+  if (obj === null) {
+  	result = "null";
+  } else if (typeof(obj) !== 'object'){
+	  if (typeof(obj) === 'number') {
+	  	result = obj.toString();
+	  } else if (typeof(obj) === 'string') {
+	  	result = '"' + obj + '"';
+	  } else if (typeof(obj) === 'boolean') {
+	  	  if (obj === true) {
+		  	result = "true";
+		  } else {
+		  	result = "false";
+		  }
+	  }
+	}
+  else if (typeof(obj) === 'object') {
+    if (obj.length === undefined) {
+    	result = [];
+    	for (var key in obj) {
+    		if ((typeof(key) === 'function') || (typeof(key) === 'undefined') || (typeof(obj[key]) === 'function') || (typeof(obj[key]) === 'undefined')) {
+    			//if not valid JSON type, do not compute anything
+    		} else {
+    			result.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+    		}
+    	}
+    	result.join(', ');
+    	result = '{' + result + '}';
+    } else if (obj.length !== undefined) {
+    	result = [];
+    	for (var i = 0; i < obj.length; i++){
+    		result.push(stringifyJSON(obj[i]));
+    	}
+    	result = '[' + result + ']';
+    }
+  }
+  return result;
 };
